bf8151e12eaf872bddd8cf56ed8662ab723bf0b8
psrivast <ThoughtWorks@PS8795.local>
2012-09-26 12:20:26 -0500

    modified cookie to be secure with SSL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecureNotepad::Application.config.secret_token = 'd82a43c1abec38f954bdd23d7350b5bfe47995ed25600b33bb56dd0e280c8dbcbf01dafaea8c72d1a627a05e4483465412f1dce2e6f94671face0075a6786a1a'
